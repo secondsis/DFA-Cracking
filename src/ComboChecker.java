6bbd7502425dee6e53ff34516d3bf9ac035ecb86
Al <84354333+secondsis@users.noreply.github.com>
2024-12-01 21:12:30 -0500

    updated combochecker and added some confirmed tried combos

--- src/ComboChecker.java
+++ src/ComboChecker.java
@@ -2,12 +2,17 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ComboChecker {
     public static void main(String[] args) throws IOException {
         // change this to smth more accessible
         Scanner scan = new Scanner(System.in);
+
+        ArrayList<String> fields = new ArrayList<>(Arrays.asList("sunflower", "dandelion", "mushroom", "blue flower", "clover",
+                "strawberry", "spider", "bamboo", "pineapple", "stump",
+                "cactus", "pumpkin", "pine tree", "rose", "pepper", "coconut", "mountain top"));
 
         System.out.println("What are your combo(s)? Separate each with a newline. And end with \".\".");
         System.out.println("Example input: ");
@@ -19,17 +24,33 @@ public class ComboChecker {
         ArrayList<String> combos = new ArrayList<>();
 
         while(true){
-            String input = scan.nextLine();
+            String input = scan.nextLine().toLowerCase();
             if(input.equals(".")) {
                 break;
             }
-            // add a check for 4 fields and add in top at the end
 
-            if(input.split(", ").length != 5) {
+            String[] comboFields = input.split(", ");
+
+            // add a check for 4 fields and add in top at the end
+            if(comboFields.length == 4) {
+                System.out.println("Included 4 fields: assuming mountain top is last");
+            } else if(input.split(", ").length != 5) {
                 System.out.println("Invalid input! Requires 5 fields.");
+                continue;
             }
 
             // add a check if all the fields are real fields and do not repeat
+            ArrayList<String> usedFields = new ArrayList<>();
+            for(String field : comboFields) {
+                if(!fields.contains(field)) {
+                    System.out.println("Invalid input! A field may not exist or was misspelled.");
+                }
+                if(usedFields.contains(field)) {
+                    System.out.println("Invalid input! A field is repeated.");
+                    continue;
+                }
+                usedFields.add(field);
+            }
 
             combos.add(input);
         }
