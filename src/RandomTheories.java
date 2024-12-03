14710dfeef8834ee61704f2e97e1af6283d58dee
Al <84354333+secondsis@users.noreply.github.com>
2024-12-02 19:36:52 -0500

    RandomTheories more customizable

--- src/RandomTheories.java
+++ src/RandomTheories.java
@@ -1,36 +1,101 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class RandomTheories {
     public static void main(String[] args) throws FileNotFoundException {
-        Scanner scan = new Scanner(new File("valid_field_combinations.txt"));
+        Scanner scan = new Scanner(new File("allCombos.txt"));
+        String fileName = "inv-ref-mot-mot.csv";
 
-        HashMap<String, Integer> fieldToZones = new HashMap<>();
+        String[] mandatoryFields = new String[]{}; // {"mushroom", "pineapple"}
+        Integer[] zoneOrder = new Integer[]{-1, -1, -1, -1};
+        String[] nectarOrder = new String[]{"invigorating", "refreshing", "motivating", "motivating"};
 
-        fieldToZones.put("sunflower", 0);
-        fieldToZones.put("dandelion", 0);
-        fieldToZones.put("mushroom", 0);
-        fieldToZones.put("blue flower", 0);
-        fieldToZones.put("clover", 0);
+        HashMap<String, Integer> fieldToZones = getFieldToZonesMap();
 
-        fieldToZones.put("strawberry", 5);
-        fieldToZones.put("spider", 5);
-        fieldToZones.put("bamboo", 5);
+        HashMap<String, String> fieldToNectars = getFieldToNectarsMap();
 
-        fieldToZones.put("pineapple", 10);
-        fieldToZones.put("stump", 10);
+        ArrayList<String> validLines = new ArrayList<>();
 
-        fieldToZones.put("cactus", 15);
-        fieldToZones.put("pumpkin", 15);
-        fieldToZones.put("pine tree", 15);
-        fieldToZones.put("rose", 15);
-        fieldToZones.put("mountain top", 25);
-        fieldToZones.put("pepper", 35);
-        fieldToZones.put("coconut", 35);
+        scannerLoop:
+            while(scan.hasNext()){
+                String line = scan.nextLine();
+                String[] parts = line.split(", ");
 
+                for(String mand : mandatoryFields) {
+                    if(!line.contains(mand)) {
+                        continue scannerLoop;
+                    }
+                }
+
+                for (int i = 0; i < zoneOrder.length; i++) {
+                    Integer zone = zoneOrder[i];
+                    if(zone == -1){
+                        continue;
+                    }
+
+                    if(!zone.equals(fieldToZones.get(parts[i]))) {
+                        continue scannerLoop;
+                    }
+                }
+
+                for (int i = 0; i < nectarOrder.length; i++) {
+                    String nectar = nectarOrder[i];
+                    if(nectar.equals("any")) {
+                        continue;
+                    }
+
+                    if(!nectar.equals(fieldToNectars.get(parts[i]))) {
+                        continue scannerLoop;
+                    }
+                }
+
+                // passed all requirements
+                validLines.add(line);
+            }
+
+        String content = String.join("\n", validLines);
+
+        try(FileWriter writer = new FileWriter(fileName)) {
+            writer.write(content);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+        System.out.println("Combos: " + validLines.size());
+    }
+
+    private static ArrayList<String> getUniqueCombos(
+            ArrayList<String> original, HashMap<String, Integer> fieldToZones, HashMap<String, String> fieldToNectars) {
+        ArrayList<String> uniqueLines = new ArrayList<>();
+        outerloop:
+            for(String l : original){
+                String[] parts = l.split(", ");
+                ArrayList<Integer> zones = new ArrayList<>();
+                ArrayList<String> nectars = new ArrayList<>();
+
+                for(String field : parts){
+                    Integer zone = fieldToZones.get(field);
+                    String nectar = fieldToNectars.get(field);
+
+                    if(!zones.contains(zone) && !nectars.contains(nectar)){
+                        zones.add(zone);
+                        nectars.add(nectar);
+                    }
+                    else {
+                        continue outerloop;
+                    }
+                }
+                uniqueLines.add(l);
+            }
+            return uniqueLines;
+    }
+
+    private static HashMap<String, String> getFieldToNectarsMap() {
         HashMap<String, String> fieldToNectar = new HashMap<>();
 
         fieldToNectar.put("sunflower", "satisfying");
@@ -53,43 +118,32 @@ public class RandomTheories {
         fieldToNectar.put("mountain top", "invigorating");
         fieldToNectar.put("pepper", "invigorating");
         fieldToNectar.put("coconut", "refreshing");
+        return fieldToNectar;
+    }
 
-        ArrayList<String> validLines = new ArrayList<>();
+    private static HashMap<String, Integer> getFieldToZonesMap() {
+        HashMap<String, Integer> fieldToZones = new HashMap<>();
 
-        while(scan.hasNext()){
-            String line = scan.nextLine();
-            if(!line.contains("mushroom") && line.contains("pineapple") && !line.contains("coconut") && !line.contains("pepper")){
-                validLines.add(line);
-            }
-        }
+        fieldToZones.put("sunflower", 0);
+        fieldToZones.put("dandelion", 0);
+        fieldToZones.put("mushroom", 0);
+        fieldToZones.put("blue flower", 0);
+        fieldToZones.put("clover", 0);
 
-        ArrayList<String> uniqueLines = new ArrayList<>();
+        fieldToZones.put("strawberry", 5);
+        fieldToZones.put("spider", 5);
+        fieldToZones.put("bamboo", 5);
 
-        outerloop:
-        for(String l : validLines){
-            String[] parts = l.split(", ");
-            ArrayList<Integer> zones = new ArrayList<>();
-            ArrayList<String> nectars = new ArrayList<>();
+        fieldToZones.put("pineapple", 10);
+        fieldToZones.put("stump", 10);
 
-            for(String field : parts){
-                Integer zone = fieldToZones.get(field);
-                String nectar = fieldToNectar.get(field);
-
-                if(!zones.contains(zone) && !nectars.contains(nectar)){
-                    zones.add(zone);
-                    nectars.add(nectar);
-                }
-                else {
-                    continue outerloop;
-                }
-            }
-            uniqueLines.add(l);
-        }
-
-        for(String l : uniqueLines){
-            System.out.println(l);
-        }
-
-        System.out.println("Combos: " + uniqueLines.size());
+        fieldToZones.put("cactus", 15);
+        fieldToZones.put("pumpkin", 15);
+        fieldToZones.put("pine tree", 15);
+        fieldToZones.put("rose", 15);
+        fieldToZones.put("mountain top", 25);
+        fieldToZones.put("pepper", 35);
+        fieldToZones.put("coconut", 35);
+        return fieldToZones;
     }
 }
